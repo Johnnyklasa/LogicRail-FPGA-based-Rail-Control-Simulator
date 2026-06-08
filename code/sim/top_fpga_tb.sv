365e42d4d81cd432bd848195538b2c054c40e0bf
Sitko Karol <karlsit@student.agh.edu.pl>
2026-06-08 23:27:40 +0200

    test: verify external manipulator logic, debouncing and route memory with top_fpga_tb simulation

--- code/sim/top_fpga_tb.sv
+++ code/sim/top_fpga_tb.sv
@@ -3,7 +3,8 @@
 module top_fpga_tb;
     logic clk_100mhz;
     logic btnC;
-    logic [15:0] sw;
+    logic [7:0] JB;
+    logic [0:0] JC;
     wire [2:0] JA;
     wire Vsync, Hsync;
     wire [3:0] vgaRed, vgaGreen, vgaBlue;
@@ -11,28 +12,37 @@ module top_fpga_tb;
     
     top_fpga dut (.*);
 
-    
+   
     initial clk_100mhz = 0;
-    always #12.5 clk_100mhz = ~clk_100mhz; 
+    always #5 clk_100mhz = ~clk_100mhz;
 
     initial begin
         
-        $dumpfile("/tmp/przebiegi_srk.vcd");
+        $dumpfile("/tmp/przebiegi_pulpit.vcd");
         $dumpvars(0, top_fpga_tb);
 
         
-        btnC = 1; sw = 0;
+        btnC = 1; JB = 0; JC = 0;
         #100 btnC = 0;
+        #100;
+
+       
+        JB[3:0] = 4'b1001; 
+
         
-      
-        #100 sw[6] = 1; sw[2:0] = 3'd3;
+        #500 JC[0] = 1; 
         
-          #80000;
         
-        $display("Zakończono symulację. Zapisano przebiegi do /tmp/przebiegi_srk.vcd");
+        #1000 JC[0] = 0; 
+
+        
+        #80000;
+
+        $display("Zakończono symulację. Zapisano przebiegi do /tmp/przebiegi_pulpit.vcd");
         $finish;
     end
 endmodule
+
 
 module clk_wiz_0 (
     input clk_in1,
